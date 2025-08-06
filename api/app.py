b84f21c68db7774098edc61c4b35ea6aaefbf19f
koguma100 <william.fralia@icloud.com>
2025-08-06 12:15:45 -0700

    deployment

--- api/app.py
+++ api/app.py
@@ -9,7 +9,3 @@ def sorting():
 @app.route('/graphs')
 def graphs():
     return render_template('graphs.html')
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
