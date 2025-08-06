bf27a5249027536fd3aae10a472a07c46c76b354
koguma100 <william.fralia@icloud.com>
2025-08-06 12:06:33 -0700

    deployment fix

--- api/app.py
+++ api/app.py
@@ -11,4 +11,5 @@ def graphs():
     return render_template('graphs.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
